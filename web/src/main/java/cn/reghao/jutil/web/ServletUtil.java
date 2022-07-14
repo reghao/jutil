aa1847c39a8d1b9b898fa586c5105d844f75f537
reghao <reghaodev@gmail.com>
2022-07-14 18:00:48 +0800

    添加 web 模块, 提供一个统一的 ServletUtil

---
+++ web/src/main/java/cn/reghao/jutil/web/ServletUtil.java
@@ -0,0 +1,70 @@
+package cn.reghao.jutil.web;
+
+import cn.reghao.jutil.jdk.serializer.JsonConverter;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * @author reghao
+ * @date 2021-06-02 13:16:58
+ */
+public class ServletUtil {
+    public static Long getUserId() {
+        String userId = getRequest().getHeader("x-user-id");
+        if (userId != null) {
+            return Long.parseLong(userId);
+        }
+
+        return 0L;
+    }
+
+    public static HttpSession getSession() {
+        return getRequest().getSession();
+    }
+
+    public static Object getBody(HttpServletRequest servletRequest, Class<?> clazz) throws IOException {
+        StringBuilder body = new StringBuilder();
+        BufferedReader reader = servletRequest.getReader();
+        String line;
+        while (null != (line = reader.readLine())) {
+            body.append(line);
+        }
+        reader.close();
+        return JsonConverter.jsonToObject(body.toString(), clazz);
+    }
+
+    public static String getSessionId() {
+        return getRequest().getSession().getId();
+    }
+
+    /**
+     * 获取 query 参数值
+     *
+     * @param
+     * @return
+     * @date 2021-06-02 下午1:19
+     */
+    public static String getRequestParam(String param, String defaultValue){
+        String parameter = getRequest().getParameter(param);
+        return StringUtils.isEmpty(parameter) ? defaultValue : parameter;
+    }
+
+    public static HttpServletRequest getRequest(){
+        return getServletRequest().getRequest();
+    }
+
+    public static HttpServletResponse getResponse(){
+        return getServletRequest().getResponse();
+    }
+
+    private static ServletRequestAttributes getServletRequest(){
+        return (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+    }
+}
