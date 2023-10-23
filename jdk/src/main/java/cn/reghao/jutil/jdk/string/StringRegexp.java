7d91cf43a70cd135e2f59043d805858d3ca79ae9
reghao <reghaodev@gmail.com>
2023-10-23 19:26:54 +0800

    修改 email 正则表达式

--- jdk/src/main/java/cn/reghao/jutil/jdk/string/StringRegexp.java
+++ jdk/src/main/java/cn/reghao/jutil/jdk/string/StringRegexp.java
@@ -9,7 +9,7 @@ import java.util.regex.Pattern;
 public class StringRegexp {
     private static final Pattern mobilePattern = Pattern.compile("^1(3\\d|4[5-9]|5[0-35-9]|6[2567]|7[0-8]|8\\d|9[0-35-9])\\d{8}$");
     private static final Pattern emailPattern1 = Pattern.compile("^[A-Za-z0-9\\u4e00-\\u9fa5]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$");
-    private static final Pattern emailPattern = Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+. [a-zA-Z]{2,}$");
+    private static final Pattern emailPattern = Pattern.compile("^s*\\w+(?:\\.{0,1}[\\w-]+)*@[a-zA-Z0-9]+(?:[-.][a-zA-Z0-9]+)*\\.[a-zA-Z]+\\s*$");
     private static final Pattern ipv4Pattern = Pattern.compile("^((2((5[0-5])|([0-4]\\d)))|([0-1]?\\d{1,2}))(\\.((2((5[0-5])|([0-4]\\d)))|([0-1]?\\d{1,2}))){3}$ ");
 
     public static boolean matchMobile(String str) {
