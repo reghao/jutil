28d0dc6704d9d71b46166db40ff3bbecd8cf4deb
reghao <reghaodev@gmail.com>
2025-10-28 09:58:58 +0800

    jdk11 升级到 jdk17
    1.javax.servlet 更改为 jakarta.servlet

--- web/src/main/java/cn/reghao/jutil/web/ServletUtil.java
+++ web/src/main/java/cn/reghao/jutil/web/ServletUtil.java
@@ -7,11 +7,11 @@ import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.ServletRequest;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Enumeration;
--- web/src/main/java/cn/reghao/jutil/web/log/NginxLog.java
+++ web/src/main/java/cn/reghao/jutil/web/log/NginxLog.java
@@ -1,7 +1,6 @@
 package cn.reghao.jutil.web.log;
 
 import com.google.gson.annotations.SerializedName;
-import lombok.Getter;
 
 import java.io.Serializable;
 
@@ -9,7 +8,6 @@ import java.io.Serializable;
  * @author reghao
  * @date 2023-11-07 14:58:07
  */
-@Getter
 public class NginxLog implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -27,4 +25,60 @@ public class NginxLog implements Serializable {
     @SerializedName("http_x_forwarded_for") private String httpXForwardedFor;
     @SerializedName("http_referer") private String httpReferer;
     @SerializedName("http_user_agent") private String httpUserAgent;
+
+    public String getTimeIso8601() {
+        return timeIso8601;
+    }
+
+    public String getRemoteAddr() {
+        return remoteAddr;
+    }
+
+    public String getRequest() {
+        return request;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public String getRequestMethod() {
+        return requestMethod;
+    }
+
+    public Integer getBodyBytesSent() {
+        return bodyBytesSent;
+    }
+
+    public Double getRequestTime() {
+        return requestTime;
+    }
+
+    public String getUpstreamResponseTime() {
+        return upstreamResponseTime;
+    }
+
+    public String getUpstreamAddr() {
+        return upstreamAddr;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getHttpXForwardedFor() {
+        return httpXForwardedFor;
+    }
+
+    public String getHttpReferer() {
+        return httpReferer;
+    }
+
+    public String getHttpUserAgent() {
+        return httpUserAgent;
+    }
 }
